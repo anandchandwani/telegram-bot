5f32c51944bef39fcd591a33d1d7eaa83d72fe38
unknown <aanandchamp@gmail.com>
2019-03-09 10:19:52 +0530

    new

--- app.js
+++ app.js
@@ -24,36 +24,43 @@ app.get('/webhook/',(req,res)=>{
         }
  });
  // Creates the endpoint for our webhook
- app.post('/webhook', function(req, res) {
- 	let messaging_events = req.body.entry[0].messaging
- 	for (let i = 0; i < messaging_events.length; i++) {
- 		let event = messaging_events[i]
- 		let sender = event.sender.id
- 		if (event.message && event.message.text) {
- 			let text = event.message.text
- 			sendText(sender, "Hello");
- 		}
- 	}
- 	res.sendStatus(200)
- })
 
- function sendText(sender, text) {
- 	let messageData = {text: text}
- 	request({
- 		url: "https://graph.facebook.com/v2.6/me/messages",
- 		qs : {access_token:"EAAeQLRJqUqcBAC1HgvLDJBh5Odwcjw6etzR1eSEF0aWZBQTfLYmajtTPVxPxXwmbXMzC3JlP27ZBCEV9nWDZA7v8OupC4c05ksige49mODV5CxCfgpcAGD135BEXihUBlSgFuLGbKpf3c3kQI8UOZCIL5QHP1yhG5QzLnyQuPgZDZD"},
- 		method: "POST",
- 		json: {
- 			recipient: {id: sender},
- 			message : messageData,
- 		}
- 	}, function(error, response, body) {
- 		if (error) {
- 			console.log("sending error")
- 		} else if (response.body.error) {
- 			console.log("response body error")
- 		}
- 	})
- }
 
+const token = "EAAeQLRJqUqcBAC1HgvLDJBh5Odwcjw6etzR1eSEF0aWZBQTfLYmajtTPVxPxXwmbXMzC3JlP27ZBCEV9nWDZA7v8OupC4c05ksige49mODV5CxCfgpcAGD135BEXihUBlSgFuLGbKpf3c3kQI8UOZCIL5QHP1yhG5QzLnyQuPgZDZD";
+app.post('/webhook/', function(req, res) {
+    var messaging_events = req.body.entry[0].messaging;
+    for (var i = 0; i < messaging_events.length; i++) {
+        var event = req.body.entry[0].messaging[i];
+        var sender = event.sender.id;
+        if (event.message && event.message.text) {
+            var text = event.message.text;
+            sendTextMessage(sender, text + "!");
+        }
+    }
+    res.sendStatus(200);
+});
+function sendTextMessage(sender, text) {
+    var messageData = {
+        text: text
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: token
+        },
+        method: 'POST',
+        json: {
+            recipient: {
+                id: sender
+            },
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error:', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
 app.listen(process.env.PORT || 5000)
