f01a7e3e047e667ffa2f66d4a6cda619ba3e044f
unknown <aanandchamp@gmail.com>
2019-03-13 16:23:36 +0530

    new

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  bot.sendMessage(chatId, 'Hello');
 });
 
 app.listen(app.get('port'), function() {
